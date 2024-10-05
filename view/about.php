8c80f4902e3d3ee91609c302950f41a85ac0d574
xKensho47 <e.damiantripodi@yahoo.com>
2024-10-05 01:04:20 -0300

    Add seccion de techs
    Se agregó en about.php una sección de tecnologías, con sus respectivas animaciones.

--- view/about.php
+++ view/about.php
@@ -23,19 +23,41 @@ include "templates/header.php";
                 </div>
                 <hr>
                 <div class="info-data">
-                    <ul class="info-list">
-                        <li class="info-item"><label for="edad" class="label-item">Edad: </label>32</li>
-                        <li class="info-item"><label for="mail" class="label-item">Mail:
-                            </label><img src="../images/email.png" alt="Mi correo electrónico">
-                            </li>
-                        <li class="info-item"><label for="titulo" class="label-item">Título: </label>Técnico
-                            Universitario en Programación</li>
-                        <li class="info-item"><label for="ubicacion" class="label-item">Dirección:
-                            </label>Ciudadela, GBA Oeste - Argentina</li>
-                    </ul>
-                    <div class="info-button">
-                        <button class="download-button" id="descargaCurriculum">Descargar CV</button>
-                    </div>
+                    <section class="data-personal move" id="data-personal">
+                        <button class="right" id="right"><i class="bi bi-caret-right-fill"></i></button>
+                        <ul class="info-list">
+                            <li class="info-item"><label for="edad" class="label-item">Edad: </label>32</li>
+                            <li class="info-item"><label for="mail" class="label-item">Mail:
+                                </label><img src="../images/email.png" alt="Mi correo electrónico">
+                                </li>
+                            <li class="info-item"><label for="titulo" class="label-item">Título: </label>Técnico
+                                Universitario en Programación</li>
+                            <li class="info-item"><label for="ubicacion" class="label-item">Dirección:
+                                </label>Ciudadela, GBA Oeste - Argentina</li>
+                        </ul>
+                        <div class="info-button">
+                            <button class="download-button" id="descargaCurriculum">Descargar CV</button>
+                        </div>
+                    </section>
+                    <section class="techs " id="techs">
+                        <h3 class="techs-title">Lenguajes y tecnologías:</h3>                        
+                        <div class="techs-container">
+                            <button class="left" id="left"><i class="bi bi-caret-left-fill"></i></button>
+                            <div class="tech01"><img src="../images/techs/c-original.svg" alt=""></div>
+                            <div class="tech02"><img src="../images/techs/cplusplus.svg" alt=""></div>
+                            <div class="tech03"><img src="../images/techs/csharp.svg" alt=""></div>
+                            <div class="tech04"><img src="../images/techs/html5.svg" alt=""></div>
+                            <div class="tech05"><img src="../images/techs/css3.svg" alt=""></div>
+                            <div class="tech06"><img src="../images/techs/javascript.svg" alt=""></div>
+                            <div class="tech07"><img src="../images/techs/php.svg" alt=""></div>
+                            <div class="tech08"><img src="../images/techs/github.svg" alt=""></div>
+                            <div class="tech09"><img src="../images/techs/git.svg" alt=""></div>
+                            <div class="tech10"><img src="../images/techs/python.svg" alt=""></div>
+                            <div class="tech11"><img src="../images/techs/java.svg" alt=""></div>
+                            <div class="tech12"><img src="../images/techs/mysql.svg" alt=""></div>
+                            <div class="tech13"><img src="../images/techs/oracle.svg" alt=""></div>
+                        </div>
+                    </section>
                 </div>
             </aside>
         </article>
@@ -43,4 +65,5 @@ include "templates/header.php";
 </main>
 
 <script src="../script/downloadCurriculum.js"></script>
+<script src="../script/moverInfo.js"></script>
 </div><?php include "templates/footer.php"; ?>
